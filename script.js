6c0704b0360f7ef875c32d52cb093454b430cae4
Ben Clark <brclark15@gmail.com>
2019-10-10 09:38:00 -0500

    Set up exercise

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
 function init() {
-  fetch("https://jsonplaceholder.typicode.com/comments").then( function(response) {
-    response.json().then( function(json) {
-      const div = document.getElementById("comments");
-      div.innerHTML = "";
-      for (let i = 0; i < json.length; i++) {
-        div.innerHTML += `
-          <div class="comment">
-            <ul>
-              <li>PostID ${json[i].postId}</li>
-              <li>CommentID ${json[i].id}</li>
-              <li>Name ${json[i].name}</li>
-              <li>Email ${json[i].email}</li>
-              <li>Body ${json[i].body}</li>
-            </ul>
-          </div>
-        `;
-      }
-    });
-});
+  // Add Code for retrieving comments using fetch() from https://jsonplaceholder.typicode.com/
+  // Then code code for adding the comment JSON values to the <div id="comments"> div using a for loop
 }
 
 window.onload = init;
