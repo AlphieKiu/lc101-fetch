92b7cedd3204b5589ebc587c65cf45cf04b31c78
AlphieKiu <kiuhuaien87@gmail.com>
2019-10-10 11:20:08 -0500

    added stuff

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function init() {
-  // Add Code for retrieving comments using fetch() from https://jsonplaceholder.typicode.com/
-  // Then code code for adding the comment JSON values to the <div id="comments"> div using a for loop
+    // Add Code for retrieving comments using fetch() from https://jsonplaceholder.typicode.com/
+    fetch("https://jsonplaceholder.typicode.com/comments").then(function(response) {
+        response.json().then(function(json) {
+            let div = document.getElementById("comments")
+            for (let i = 0; i < json.length; i++) {
+                div.innerHTML += `
+                <div class="comments">
+                  <ul>
+                    <li>Post ID: ${json[i].postID}</li>
+                    <li>Comment ID: ${json[i].id}</li>
+                    <li>Name: ${json[i].name}</li>
+                    <li>Email: ${json[i].email}</li>
+                    <li>Comment: ${json[i].body}</li>
+                  </ul>
+                </div>
+            `
+            }
+        });
+    });
+    // Then code code for adding the comment JSON values to the <div id="comments"> div using a for loop
 }
 
 window.onload = init;
